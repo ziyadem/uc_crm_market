263168b6dc504625758938cf330ea216b550c64f
ziyadem <maxkamovaziyaem17@gmail.com>
2024-07-05 01:35:13 +0500

    fix: conflict

--- frontend/src/pages/statistika/index.jsx
+++ frontend/src/pages/statistika/index.jsx
@@ -1,8 +1,7 @@
-import StatistikaList from './components/StatistikaList'
-import Back from '../../components/Back'
+import Back from "../../components/back";
+import StatistikaList from "./components/StatistikaList";
 
 const Statistika = () => {
-
   return (
     <section className="p-4 page">
       <div className="border border-secondary border-opacity-50 rounded-5 bg-secondary p-4 bg-opacity-25 h-100">
@@ -10,7 +9,7 @@ const Statistika = () => {
         <StatistikaList />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Statistika
+export default Statistika;
